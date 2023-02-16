1b0e475f5677d102643c0e54548d34c62446269f
Daems033 <damian14gil@gmail.com>
2023-02-16 18:20:56 +0100

    cookies

--- Proyecto_AAA/index.js
+++ Proyecto_AAA/index.js
@@ -4,29 +4,19 @@ let date = new Date(Date.now() + 86400e3);
 date = date.toUTCString();
 document.cookie = "usuario=John; expires=" + date + "; path=/; secure";
 
-setTimeout(() => {
-  alert(document.cookie);
-}, 3000);
-
-
-/*
 const aceptarCookies = document.querySelector(".aceptar-cookies");
 
 aceptarCookies.addEventListener("click", function () {
-    document.cookie = "user=John";
-    alert(document.cookie);
-
-  document.querySelector(".banner-cookies").style.display = "none";
-  localStorage.setItem("aceptarCookies", true);
-  let fecha = new Date(new Date().getTime() + 3600 * 1000 * 24 * 365);
-  document.cookie = "username=aceptarCookies; expires=" + fecha.toUTCString() + "; path=" + "/";
-  localStorage.setItem("aceptarCookies", valor);
+    document.querySelector(".banner-cookies").style.display = "none";
+    let date = new Date(Date.now() + 86400e3);
+    date = date.toUTCString();
+    document.cookie = "usuario=Damian; expires=" + date + "; path=/; secure";
 });
 
 if (document.cookie.split(';').some((item) => item.trim().startsWith('aceptarCookies=true'))) {
-  document.querySelector(".banner-cookies").style.display = "none";
+    document.querySelector(".banner-cookies").style.display = "none";
 }
 
 if (localStorage.getItem("aceptarCookies")) {
     document.querySelector(".banner-cookies").style.display = "none";
-}*/
+}
