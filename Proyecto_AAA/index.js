4735e8f5f7e93870bf3985ae3e0c48a4964056d5
Daems033 <damian14gil@gmail.com>
2023-02-16 18:40:46 +0100

    cookies

--- Proyecto_AAA/index.js
+++ Proyecto_AAA/index.js
@@ -1,22 +1,19 @@
 'use strict'
 
-let date = new Date(Date.now() + 86400e3);
-date = date.toUTCString();
-document.cookie = "usuario=John; expires=" + date + "; path=/; secure";
-
 const aceptarCookies = document.querySelector(".aceptar-cookies");
 
 aceptarCookies.addEventListener("click", function () {
     document.querySelector(".banner-cookies").style.display = "none";
     let date = new Date(Date.now() + 86400e3);
     date = date.toUTCString();
-    document.cookie = "usuario=Damian; expires=" + date + "; path=/; secure";
+    document.cookie = "usuario=miCookie; expires=" + date + "; path=/; secure";
 });
 
+/*
 if (document.cookie.split(';').some((item) => item.trim().startsWith('aceptarCookies=true'))) {
     document.querySelector(".banner-cookies").style.display = "none";
 }
 
 if (localStorage.getItem("aceptarCookies")) {
     document.querySelector(".banner-cookies").style.display = "none";
-}
+}*/
