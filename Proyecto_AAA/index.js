b32d4662387276da29b8c084f041ffea520f96f4
Daems033 <damian14gil@gmail.com>
2023-02-16 20:12:55 +0100

    carrusel

--- Proyecto_AAA/index.js
+++ Proyecto_AAA/index.js
@@ -9,11 +9,49 @@ aceptarCookies.addEventListener("click", function () {
     document.cookie = "usuario=miCookie; expires=" + date + "; path=/; secure";
 });
 
-/*
-if (document.cookie.split(';').some((item) => item.trim().startsWith('aceptarCookies=true'))) {
-    document.querySelector(".banner-cookies").style.display = "none";
-}
+// Variables
+let aside = document.querySelector("aside");
+let testimonials = [
+  {
+    content:
+      "A programmer who writes clean code fully understands the problem before writing the code.",
+    audio: "audios/home.mp3",
+    img: "imagenes/Selección_072.png",
+    name: "DAMIAN GIL",
+    job: "Programmer",
+  },
+  {
+    content:
+      "Graphic design should seduce, educate and, perhaps most importantly, provoke an emotional response.",
+    audio: "audios/home.mp3",
+    img: "imagenes/Selección_072.png",
+    name: "KYLE WALKER",
+    job: "Designer",
+  }
+];
 
-if (localStorage.getItem("aceptarCookies")) {
-    document.querySelector(".banner-cookies").style.display = "none";
-}*/
+// Funciones
+let createCarousel = function (arr) {
+  let i = 0;
+  let carouselInterval = setInterval(function () {
+    aside.innerHTML = `
+    <h2>TESTIMONIALS</h2>
+    <img src="${arr[i].img}" alt="Comillas">
+    <p>${arr[i].content}</p>
+    <audio controls style="margin-bottom:4em;" volume="1.0">
+        <source src="${arr[i].audio}" type="audio/mpeg">
+    </audio> 
+    <span>${arr[i].name}</span>
+    <span>${arr[i].job}</span>
+    `;
+    i++;
+    if (i == arr.length) {
+      i = 0;
+    }
+  }, 3000);
+};
+
+
+// Llamado de Función
+createCarousel(testimonials);
+
